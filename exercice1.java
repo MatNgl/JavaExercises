26ce918355fb6b4947b72233f1340d3788cd2625
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 14:21:49 +0200

    Add files via upload

--- exercice1.java
+++ exercice1.java
@@ -1,8 +1,29 @@
-public class exercices {
+import java.util.ArrayList;
+
+public class exercice1 {
+
     public static void main(String[] args) {
-        String name = "Alex";
-        Integer age = 25;
-        System.out.println(name + age);
-    
+        ArrayList<Integer> stock = new ArrayList<>();
+        stock.add(14);
+        stock.add(887);
+        stock.add(58);
+        stock.add(713);
+        stock.add(179);
+        stock.add(512);
+        stock.add(786);
+        stock.add(29);
+
+        int total = sum(stock);
+
+        System.out.println("Contenu du stock : " + total);
+    }
+
+    private static int sum(ArrayList<Integer> stock) {
+        int total = 0;
+        for (int number : stock) {
+            total += number;
+        }
+        return total;
     }
 }
+
