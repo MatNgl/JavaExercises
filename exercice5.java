e02e73fa3499bc18c6ceaeee69f101bdb08fd18d
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 11:16:09 +0200

    Add files via upload

---
+++ exercice5.java
@@ -0,0 +1,14 @@
+public class exercice5{
+    public static void main (String[] args){
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        int i = 1; 
+
+        while (i < str.length()){
+            
+            System.out.print(str.charAt(i));
+            i= i+2;
+        }
+
+    }
+}
+
