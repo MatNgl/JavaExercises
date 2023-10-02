032113444c000c7f6961b156ba5376c47720b659
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 10:06:13 +0200

    Add files via upload

---
+++ exercice3.java
@@ -0,0 +1,11 @@
+public class exercice3 {
+    public static void main (String[] args) {
+
+        String name = "Halil";
+        Integer age = 27;
+        String job = "développeur";
+
+        System.out.println("Je m'appelle" + name + "j'ai" + age + "et je suis" + job);
+
+    }
+}
