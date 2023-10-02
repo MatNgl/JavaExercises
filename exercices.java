5842c45aa30f5e3092984819cb54329408a7ed9a
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 09:50:13 +0200

    Add files via upload

---
+++ exercices.java
@@ -0,0 +1,8 @@
+public class exercices {
+    public static void main(String[] args) {
+        String name = "Alex";
+        Integer age = 25;
+        System.out.println(name + age);
+    
+    }
+}
