878086cc28f71dbae050c255561198c31d291654
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 11:36:17 +0200

    Add files via upload

---
+++ exercice7.java
@@ -0,0 +1,9 @@
+public class exercice7{
+    public static void main (String[] args){
+        int i = 0;
+        for (i = 0; i<=10; i++){
+            int resultat = i*5;
+            System.out.println(i + "x 5 = " + resultat);
+        }
+    }
+}
