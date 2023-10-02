e20a5f342c3b16d99f43d0f4cd543076b92fb98f
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 10:01:43 +0200

    Add files via upload

---
+++ exercice2.java
@@ -0,0 +1,10 @@
+public class exercice2 {
+    public static void main (String[] args) {
+        double price = 19.99;
+        Integer quantity = 12;
+        double total = quantity + price ;
+
+        System.out.println(total);
+
+    }
+}
