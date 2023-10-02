4ef0351a6608ac9a11d556e30c189355112c9669
MatNgl <99647291+MatNgl@users.noreply.github.com>
2023-10-02 12:00:34 +0200

    Add files via upload

---
+++ exercice8.java
@@ -0,0 +1,16 @@
+public class exercice8{
+    public static void main (String[] args){
+        String[] colors = {"bleu", "rouge", "jaune"};
+        for(String value : colors){
+            
+
+
+
+        System.out.println("La couleur préférée de Louis est le " + value +
+        ". Pour sa peinture, Andrée a utilisé du " + value +
+        ". Anis n'a pas utilisé de chemise " +value);
+        }
+    }
+}
+
+// pas réussi 
